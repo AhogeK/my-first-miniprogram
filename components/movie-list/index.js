060aaa3956bcdcad1a2b0db2cf498bf11c44598e
AhogeK <ahogek@gmail.com>
2021-03-10 15:45:14 +0800

    ✨feature: 电影模块结构搭建

---
+++ components/movie-list/index.js
@@ -0,0 +1,23 @@
+// components/movie-list/index.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
