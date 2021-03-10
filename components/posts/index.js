9a6c5c060885091ada8a5760ff53d2dc340405b9
AhogeK <ahogek@gmail.com>
2021-03-10 15:19:11 +0800

    ✨feature: 将 post 组件化

---
+++ components/posts/index.js
@@ -0,0 +1,23 @@
+// components/posts/index.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    postData: Object
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
