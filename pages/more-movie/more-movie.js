bf95e042ac3e83b29964d6918511b78833e04c4a
AhogeK <ahogek@gmail.com>
2021-03-11 16:00:08 +0800

    ✨feature: 更多电影模块

--- pages/more-movie/more-movie.js
+++ pages/more-movie/more-movie.js
@@ -1,25 +1,55 @@
 // pages/more-movie/more-movie.js
+
+const app = getApp()
+
 Page({
 
   /**
    * Page initial data
    */
   data: {
-
+    movies: [],
+    _type: ''
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-
+    this.data._type = options.type
+    wx.request({
+      url: app.gBaseUrl + this.data._type,
+      data: {
+        start: 0,
+        count: 12
+      },
+      success: (res) => {
+        this.setData({
+          movies: res.data.subjects
+        })
+      }
+    })
   },
 
   /**
    * Lifecycle function--Called when page is initially rendered
    */
   onReady: function () {
-
+    let title = '电影'
+    switch(this.data._type) {
+      case 'in_theaters':
+        title = '正在热映'
+        break
+      case 'coming_soon':
+        title = '即将上映'
+        break
+      case 'top250':
+        title = '豆瓣Top250'
+        break
+    }
+    wx.setNavigationBarTitle({
+      title
+    })
   },
 
   /**
@@ -47,14 +77,39 @@ Page({
    * Page event handler function--Called when user drop down
    */
   onPullDownRefresh: function () {
-
+    wx.request({
+      url: app.gBaseUrl + this.data._type,
+      data: {
+        start: 0,
+        count: 12
+      },
+      success: (res) => {
+        this.setData({
+          movies: res.data.subjects
+        })
+        wx.stopPullDownRefresh()
+      }
+    })
   },
 
   /**
    * Called when page reach bottom
    */
   onReachBottom: function () {
-
+    wx.showNavigationBarLoading()
+    wx.request({
+      url: app.gBaseUrl + this.data._type,
+      data: {
+        start: this.data.movies.length,
+        count: 12
+      },
+      success: (res) => {
+        this.setData({
+          movies:this.data.movies.concat(res.data.subjects)
+        })
+      }
+    })
+    wx.hideNavigationBarLoading()
   },
 
   /**
